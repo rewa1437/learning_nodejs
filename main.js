a189e5976da8f719cdb4368f86cfe5703e76f71a
rewa1437 <148621784+rewa1437@users.noreply.github.com>
2025-09-07 23:05:23 +0700

    promise

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
+const { rejects } = require('assert')
 const {writeFile} = require('fs')
+const { resolve } = require('path')
 
-writeFile('future.txt','anupap pasakornhiran',null,(err)=>{
-    if (err) {
-        console.error('error => ', err);
-        return;
-    }
+// writeFile('future.txt','anupap pasakornhiran',null,(err)=>{
+//     if (err) {
+//         console.error('error => ', err);
+//         return;
+//     }
 
-    console.log('write file complete')
-    function add(a,b){
-        return a + b;
-    }
-    console.log('test ', add(1,4))
-})
+//     console.log('write file complete')
+//     function add(a,b){
+//         return a + b;
+//     }
+//     console.log('test ', add(1,4))
+// })
+
+
+function writefileprom() {
+    return new Promise((resolve,rejects)=>{
+        writeFile('future.txt','anupap pasakornhiran',null,(err)=>{
+            if (err) {
+                rejects(err)
+                return
+            }
+            resolve('writefile complete')
+        })
+    })
+}
+
+function add(a,b){
+    return a + b;
+}
+
+async function main(){
+    const writefileres = await writefileprom();
+    console.log('res =>',writefileres);
+    console.log('test',add(1,99))
+}
+
+main();
